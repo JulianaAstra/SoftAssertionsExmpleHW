a4044e598e6d331cfa2c8bf28c49c6b22cd4d4ba
julia karnausova <julianaas@yandex.ru>
2025-08-17 02:24:53 +0300

    add steps to SoftAssertions page and check option in menu

--- src/test/java/tests/SoftAssertionsExampleTest.java
+++ src/test/java/tests/SoftAssertionsExampleTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SoftAssertionsExampleTest {
@@ -11,8 +13,8 @@ public class SoftAssertionsExampleTest {
 
     @BeforeAll
     static void setupConfig() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1560x900";
+        Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
     }
 
@@ -23,7 +25,15 @@ public class SoftAssertionsExampleTest {
 
     @Test
     void CheckExamplpeTest() {
-        route = "/automation-practice-form";
+        route = "/selenide/selenide";
         open(route);
+        $("#wiki-tab").click();
+        $("#wiki-pages-box button.Link--muted").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+
+        $("#wiki-pages-box").$("details:last-child").click();
+        $("#wiki-pages-box").$$("a").findBy(text("SoftAssertions")).click();
+
+        $$("h1").findBy(visible).shouldHave(text("SoftAssertions"));
     }
 }
